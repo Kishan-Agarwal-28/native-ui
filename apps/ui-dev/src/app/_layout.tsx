1d98bce65e20758ec4fc91e443d6c304acc7e2b0
Kishan Agarwal <kishanagarwal028@gmail.com>
2026-06-20 19:06:23 +0530

    feat/enchanced the theme provider and also added a persist state implemented using mmkv

--- apps/ui-dev/src/app/_layout.tsx
+++ apps/ui-dev/src/app/_layout.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "@/lib/theme";
 import { Stack } from "expo-router";
-
+import { PortalHost } from "@rn-primitives/portal";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider persist={false}>
+        <Stack screenOptions={{ headerShown: false }} />
+        <PortalHost />
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
 }
--- apps/ui-dev/src/app/index.tsx
+++ apps/ui-dev/src/app/index.tsx
@@ -1,17 +1,36 @@
-import { Text, View, StyleSheet } from "react-native";
+import Badge from "@/components/ui/badge";
+import ThemeToggle from "@/components/ui/theme-toggle";
+
+import { THEME } from "@/lib/theme";
+import useStyles from "@/lib/use-styles";
+import { Text, View } from "react-native";
 
 export default function Index() {
+  const styles = useStyles((t) => ({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: THEME[t].colors.card,
+    },
+    text: {
+      fontSize: THEME[t].typography.base,
+      color: THEME[t].colors.foreground,
+    },
+  }));
   return (
     <View style={styles.container}>
+      <ThemeToggle
+        type="toggle"
+        animate
+        showDateTimeOptions
+        timeRanges={{
+          dayStart: "10:00",
+          nightStart: "11:00",
+        }}
+      />
+      <Badge label="Default" variant="default" />
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
